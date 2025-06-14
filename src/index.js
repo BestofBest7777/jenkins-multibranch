65afea2cc1bdf543a09558e407f97e57aff47609
Sobitov Shamsiddin <sobitov2003shamsiddin@gmail.com>
2025-06-14 12:40:48 +0000

    Add Jenkinsfile and update index.js for dev branch

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Добавляем вывод REACT_APP_PORT
+console.log("App running on port:", process.env.REACT_APP_PORT);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
